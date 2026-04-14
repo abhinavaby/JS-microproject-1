675767ca92dd5237e96c255784d7c7fd2fa813be
abhinavaby <abhinavaby07@gmail.com>
2026-04-14 18:42:45 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,42 @@
+function calculate(){
+    const a=document.getElementById("d").value;
+    console.log(a);
+    
+    if(a=="black"){
+        document.body.style.backgroundColor="black";
+        document.body.style.color="white";
+    }
+    else if(a=="white"){
+        document.body.style.backgroundColor="white";
+        document.body.style.color="black";
+    }
+    else if(a=="red"){
+        document.body.style.backgroundColor="red";
+        document.body.style.color="white";
+    }
+    else if(a=="blue"){
+        document.body.style.backgroundColor="blue";
+        document.body.style.color="white";
+    }
+    else if(a=="green"){
+        document.body.style.backgroundColor="green";
+        document.body.style.color="white";
+    }
+    else if(a=="yellow"){
+        document.body.style.backgroundColor="yellow";
+        document.body.style.color="black";
+    }
+    else if(a=="pink"){
+        document.body.style.backgroundColor="pink";
+        document.body.style.color="black";
+    }
+    else if(a=="purple"){
+        document.body.style.backgroundColor="purple";
+        document.body.style.color="white";
+    }
+    else if(a=="orange"){
+        document.body.style.backgroundColor="orange";
+        document.body.style.color="black";
+    }
+
+}
